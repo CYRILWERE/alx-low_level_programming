b5836c749edefc218d7cc5cc3feca9a5ffbb470d
Cyrilwere <CYRILWERE>
2023-08-24 12:08:36 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -13,4 +13,21 @@ char *cap_string(char *str)
 		while (!(str[index] >= '0' && str[index] <= '0'))
 			index++;
 		if (str[index - 1] == ' ' ||
-				
+			str[index - 1] == '\t' ||
+		str[index - 1] == '\n' ||
+	str[index - 1] == ',' ||
+str[index - 1] == ';' ||
+str[index - 1] == '.' ||
+str[index - 1] == '!' ||
+str[index - 1] == '?' ||
+str[index - 1] == '"' ||
+str[index - 1] == '(' ||
+str[index - 1] == ')' ||
+str[index - 1] == '{' ||
+str[index - 1] == '}' ||
+index == 0)
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
