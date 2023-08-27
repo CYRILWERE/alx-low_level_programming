6934e62d1b2b4fca5cdb5f2ed86faf4c6a90a7f6
Cyrilwere <CYRILWERE>
2023-08-27 12:21:51 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,22 +10,22 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= '0' && str[index] <= '0'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 		if (str[index - 1] == ' ' ||
-			str[index - 1] == '\t' ||
+		str[index - 1] == '\t' ||
 		str[index - 1] == '\n' ||
-	str[index - 1] == ',' ||
-str[index - 1] == ';' ||
-str[index - 1] == '.' ||
-str[index - 1] == '!' ||
-str[index - 1] == '?' ||
-str[index - 1] == '"' ||
-str[index - 1] == '(' ||
-str[index - 1] == ')' ||
-str[index - 1] == '{' ||
-str[index - 1] == '}' ||
-index == 0)
+		str[index - 1] == ',' ||
+		str[index - 1] == ';' ||
+		str[index - 1] == '.' ||
+		str[index - 1] == '!' ||
+		str[index - 1] == '?' ||
+		str[index - 1] == '"' ||
+		str[index - 1] == '(' ||
+		str[index - 1] == ')' ||
+		str[index - 1] == '{' ||
+		str[index - 1] == '}' ||
+		index == 0)
 			str[index] -= 32;
 		index++;
 	}
