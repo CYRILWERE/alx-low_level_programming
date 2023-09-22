7feff4d821bc7bc9f6a17a1b79d5bb2dab93ad3a
Cyrilwere <CYRILWERE>
2023-09-22 12:54:36 -0700

    64 bit

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+extern void asm_hello_holberton(); // Declare the assembly function
+
+int main(void) {
+    asm_hello_holberton(); // Call the assembly function
+    return 0;
+}
